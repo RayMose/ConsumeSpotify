84c4e233eae757c21c5e5eedfc83ba696ef0f615
Yassin Musa <yassinmusa17@gmail.com>
2024-06-19 06:09:25 +0300

    Fixed bugAdd project files.

---
+++ Services/SpotifyAccount.cs
@@ -0,0 +1,18 @@
+﻿
+namespace ConsumeSpotify.Services
+{
+    public class SpotifyAccount : ISpotifyAccount
+    {
+        private readonly HttpClient _httpClient;
+
+        public SpotifyAccount(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<string> GetToken(string clientId, string clientSecret)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
